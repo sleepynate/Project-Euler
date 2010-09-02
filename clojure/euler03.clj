971de2e430b97af4a40875cfaf5ee83deb5f2950
Nathan Dotz <nathan.dotz@gmail.com>
2010-09-02 14:45:35 -0400

    finish euler 3 in clojure. consider changing list comprehension in prime_divs to something else.

--- clojure/euler03.clj
+++ clojure/euler03.clj
@@ -6,4 +6,20 @@
 ;
 ;
 
+(ns euler3)
 
+(def bignum 600851475143)
+
+(def divisor                                                  ;only numbers who haves
+  (filter #(zero? (rem bignum %))                             ; remainder 0 when dividing bignum
+          (filter odd? (range 2 (/ (Math/sqrt bignum) 6 ) ) ) ; up to big/6
+          )
+  )
+(def prime_divs 
+  (for [x divisors :when
+        (every? #(not (zero? (rem x %)) ) ;  
+                (range 2 (Math/sqrt x)) ) ;
+        ] x ) 
+  )
+
+(last prime_divs)
