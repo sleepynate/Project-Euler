a96db5d7d4156dddddd24147274aeec89a9e62cf
Nathan Dotz <nathan.dotz@gmail.com>
2011-02-14 15:38:08 -0500

    change header in haskel #5

--- haskell/peuler05.hs
+++ haskell/peuler05.hs
@@ -1,4 +1,13 @@
--- What is the smallest number that is evenly divisible
--- by all of the numbers from 1 to 20?
+{-
+ - Project Euler - Problem 5
+ -
+ - 2520 is the smallest number that can be divided by 
+ - each of the numbers from 1 to 10 without any remainder.
+ - What is the smallest number that is evenly divisible
+ - by all of the numbers from 1 to 20?
+ -
+ - solution by nathan dotz
+ - nathan (period) dotz (at sign) gmail (period) com
+ -}
 
 main = putStrLn $ "The smallest number that is evenly divisible by all the numbers from 1 to 20 is " ++ show ( foldl lcm 1 [2..20] )
